5f35aff0941b381400d337c5893148fd3078d744
RyanJ <ryan.t.jarrett@vanderbilt.edu>
2020-12-08 11:57:21 -0600

    Added 2- and 3-compartment models.

--- R/pk_mods.R
+++ R/pk_mods.R
@@ -31,6 +31,156 @@ pkmod1cpt <- function(tm, kR, pars, init = 0, inittm = 0){
 class(pkmod1cpt) <- "pkmod"
 
 
+
+
+#' Two compartment IV infusion with first-order elimination.
+#'
+#' @param tm Vector of times to evaluate the PK function at
+#' @param kR Infusion rate (e.g. ml/min).
+#' @param pars Named vector of parameters with names ('K10','K12','K21','V1','V2') or ('CL','Q','V1','V2').
+#' @param init Initial concentration. Defaults to 0 in both compartments.
+#' @param inittm Time of initiation of infusion. Defaults to 0.
+#' @param k20 Elimination rate constant for second compartment. Defaults to 0.
+#' @examples
+#' pkmod2cpt(1,1,c(CL = 15, V1 = 10, Q = 10, V2 = 20))
+#' @export
+pkmod2cpt <- function(tm, kR, pars, init = c(0,0), inittm = 0, k20 = 0){
+
+  names(pars) <- tolower(names(pars))
+  tm <- tm - inittm
+
+  if(any(!(c("k10","k12","k21","v1","v2") %in% names(pars))) & any(!(c("cl","q","v1","v2") %in% names(pars))))
+    stop("pars must have names ('k10','k12','k21','v1','v2') or ('cl','q','v1','v2')")
+
+  v1 <- pars["v1"]
+  v2 <- pars["v2"]
+  if(all(c("cl","q") %in% names(pars))){
+    k10 <- pars["cl"] / v1
+    k12 <- pars["q"] / v1
+    k21 <- pars["q"] / v2
+  } else{
+    k10 <- pars["k10"]
+    k12 <- pars["k12"]
+    k21 <- pars["k21"]
+  }
+
+  E1 <- k10+k12
+  E2 <- k21+k20
+
+  lambda1 = 0.5*((E1+E2)+sqrt((E1+E2)^2-4*(E1*E2-k12*k21)))
+  lambda2 = 0.5*((E1+E2)-sqrt((E1+E2)^2-4*(E1*E2-k12*k21)))
+
+  A1last <- init[1]*v1
+  A2last <- init[2]*v2
+  Doserate <- kR
+
+  A1term1 = (((A1last*E2+Doserate+A2last*k21)-A1last*lambda1)*exp(-tm*lambda1)-((A1last*E2+Doserate+A2last*k21)-A1last*lambda2)*exp(-tm*lambda2))/(lambda2-lambda1)
+  A1term2 = Doserate*E2*(1/(lambda1*lambda2)+exp(-tm*lambda1)/(lambda1*(lambda1-lambda2))-exp(-tm*lambda2)/(lambda2*(lambda1-lambda2)))
+  A1term <- A1term1+A1term2 # Amount in central compartment
+
+  A2term1 = (((A2last*E1+A1last*k12)-A2last*lambda1)*exp(-tm*lambda1)-((A2last*E1+A1last*k12)-A2last*lambda2)*exp(-tm*lambda2))/(lambda2-lambda1)
+  A2term2 = Doserate*k12*(1/(lambda1*lambda2)+exp(-tm*lambda1)/(lambda1*(lambda1-lambda2))-exp(-tm*lambda2)/(lambda2*(lambda1-lambda2)))
+  A2term <- A2term1+A2term2   #Amount in the peripheral compartment
+
+  return(rbind(A1term/v1, A2term/v2))
+}
+class(pkmod2cpt) <- "pkmod"
+
+
+
+
+#' Three compartment IV infusion with first-order elimination.
+#'
+#' @param tm Vector of times to evaluate the PK function at
+#' @param kR Infusion rate (e.g. ml/min).
+#' @param pars Named vector of parameters with names ('K10','K12','K21','V1','V2') or ('CL','Q','V1','V2').
+#' @param init Initial concentration. Defaults to 0 in all compartments.
+#' @param inittm Time of initiation of infusion. Defaults to 0.
+#' @param k20 Elimination rate constant for second compartment. Defaults to 0.
+#' @param k30 Elimination rate constant for second compartment. Defaults to 0.
+#' @examples
+#' pkmod3cpt(1,1,c(CL = 15, Q12 = 10, Q13 = 5, V1 = 10, V2 = 20, V3 = 50))
+#' @export
+pkmod3cpt <- function(tm, kR, pars, init = c(0,0,0), inittm = 0, k20 = 0, k30 = 0){
+
+  names(pars) <- tolower(names(pars))
+  tm <- tm - inittm
+
+  if(any(!(c("k10","k12","k21","k13","k31","v1","v2","v3") %in% names(pars))) & any(!(c("cl","q12","q13","v1","v2","v3") %in% names(pars))))
+    stop("pars must have names ('k10','k12','k21','k13','k31','v1','v2','v3') or ('cl','q12','q21','v1','v2','v3')")
+
+  v1 <- pars["v1"]
+  v2 <- pars["v2"]
+  v3 <- pars["v3"]
+
+  if(all(c("cl","q12","q13") %in% names(pars))){
+    k10 <- pars["cl"] / v1
+    k12 <- pars["q12"] / v1
+    k21 <- pars["q12"] / v2
+    k13 <- pars["q13"] / v1
+    k31 <- pars["q13"] / v3
+  } else{
+    k10 <- pars["k10"]
+    k12 <- pars["k12"]
+    k21 <- pars["k21"]
+    k13 <- pars["k13"]
+    k31 <- pars["k31"]
+  }
+
+  E1 <- k10+k12+k13
+  E2 <- k21+k20
+  E3 <- k31+k30
+
+  #calculate hybrid rate constants
+  a <- E1+E2+E3
+  b <- E1*E2+E3*(E1+E2)-k12*k21-k13*k31
+  c <- E1*E2*E3-E3*k12*k21-E2*k13*k31
+
+  m <- (3*b - a^2)/3
+  n <- (2*a^3 - 9*a*b + 27*c)/27
+  Q <- (n^2)/4 + (m^3)/27
+
+  alpha <- sqrt(-1*Q)
+  beta <- -1*n/2
+  gamma <- sqrt(beta^2+alpha^2)
+  theta <- atan2(alpha,beta)
+
+  lambda1 <- a/3 + gamma^(1/3)*(cos(theta/3) + sqrt(3)*sin(theta/3))
+  lambda2 <- a/3 + gamma^(1/3)*(cos(theta/3) - sqrt(3)*sin(theta/3))
+  lambda3 <- a/3 -(2*gamma^(1/3)*cos(theta/3))
+
+  A1last <- init[1]*v1
+  A2last <- init[2]*v2
+  A3last <- init[3]*v3
+  Doserate <- kR
+
+  B = A2last*k21+A3last*k31
+  C = E3*A2last*k21+E2*A3last*k31
+  I = A1last*k12*E3-A2last*k13*k31+A3last*k12*k31
+  J = A1last*k13*E2+A2last*k13*k21-A3last*k12*k21
+
+  A1term1 = A1last*(exp(-tm*lambda1)*(E2-lambda1)*(E3-lambda1)/((lambda2-lambda1)*(lambda3-lambda1))+exp(-tm*lambda2)*(E2-lambda2)*(E3-lambda2)/((lambda1-lambda2)*(lambda3-lambda2))+exp(-tm*lambda3)*(E2-lambda3)*(E3-lambda3)/((lambda1-lambda3)*(lambda2-lambda3)))
+  A1term2 = exp(-tm*lambda1)*(C-B*lambda1)/((lambda1-lambda2)*(lambda1-lambda3))+exp(-tm*lambda2)*(B*lambda2-C)/((lambda1-lambda2)*(lambda2-lambda3))+exp(-tm*lambda3)*(B*lambda3-C)/((lambda1-lambda3)*(lambda3-lambda2))
+  A1term3 = Doserate*((E2*E3)/(lambda1*lambda2*lambda3)-exp(-tm*lambda1)*(E2-lambda1)*(E3-lambda1)/(lambda1*(lambda2-lambda1)*(lambda3-lambda1))-exp(-tm*lambda2)*(E2-lambda2)*(E3-lambda2)/(lambda2*(lambda1-lambda2)*(lambda3-lambda2))-exp(-tm*lambda3)*(E2-lambda3)*(E3-lambda3)/(lambda3*(lambda1-lambda3)*(lambda2-lambda3)))
+  A1term  = A1term1+A1term2+A1term3    #Amount in the central compartment
+
+  A2term1 = A2last*(exp(-tm*lambda1)*(E1-lambda1)*(E3-lambda1)/((lambda2-lambda1)*(lambda3-lambda1))+exp(-tm*lambda2)*(E1-lambda2)*(E3-lambda2)/((lambda1-lambda2)*(lambda3-lambda2))+exp(-tm*lambda3)*(E1-lambda3)*(E3-lambda3)/((lambda1-lambda3)*(lambda2-lambda3)))
+  A2term2 = exp(-tm*lambda1)*(I-A1last*k12*lambda1)/((lambda1-lambda2)*(lambda1-lambda3))+exp(-tm*lambda2)*(A1last*k12*lambda2-I)/((lambda1-lambda2)*(lambda2-lambda3))+exp(-tm*lambda3)*(A1last*k12*lambda3-I)/((lambda1-lambda3)*(lambda3-lambda2))
+  A2term3 = Doserate*k12*(E3/(lambda1*lambda2*lambda3)-exp(-tm*lambda1)*(E3-lambda1)/(lambda1*(lambda2-lambda1)*(lambda3-lambda1))-exp(-tm*lambda2)*(E3-lambda2)/(lambda2*(lambda1-lambda2)*(lambda3-lambda2))-exp(-tm*lambda3)*(E3-lambda3)/(lambda3*(lambda1-lambda3)*(lambda2-lambda3)))
+  A2term  = A2term1+A2term2+A2term3    #Amount in the first-peripheral compartment
+
+  A3term1 = A3last*(exp(-tm*lambda1)*(E1-lambda1)*(E2-lambda1)/((lambda2-lambda1)*(lambda3-lambda1))+exp(-tm*lambda2)*(E1-lambda2)*(E2-lambda2)/((lambda1-lambda2)*(lambda3-lambda2))+exp(-tm*lambda3)*(E1-lambda3)*(E2-lambda3)/((lambda1-lambda3)*(lambda2-lambda3)))
+  A3term2 = exp(-tm*lambda1)*(J-A1last*k13*lambda1)/((lambda1-lambda2)*(lambda1-lambda3))+exp(-tm*lambda2)*(A1last*k13*lambda2-J)/((lambda1-lambda2)*(lambda2-lambda3))+exp(-tm*lambda3)*(A1last*k13*lambda3-J)/((lambda1-lambda3)*(lambda3-lambda2))
+  A3term3 = Doserate*k13*(E2/(lambda1*lambda2*lambda3)-exp(-tm*lambda1)*(E2-lambda1)/(lambda1*(lambda2-lambda1)*(lambda3-lambda1))-exp(-tm*lambda2)*(E2-lambda2)/(lambda2*(lambda1-lambda2)*(lambda3-lambda2))-exp(-tm*lambda3)*(E2-lambda3)/(lambda3*(lambda1-lambda3)*(lambda2-lambda3)))
+  A3term = A3term1+A3term2+A3term3
+
+  return(rbind(A1term/v1, A2term/v2, A3term/v3))
+}
+class(pkmod3cpt) <- "pkmod"
+
+
+
+
 #' Solution to three-compartment IV model with effect-site
 #'
 #' 3 compartment IV infusion with first-order absorption between compartments and with an additional effect-site compartment.
